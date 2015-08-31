fee49c22494d2d6c18b39838d4c5e657d6b51dda
chenxy <chenxuanyu@xiaocheben.com>
2015-08-31 17:41:53 +0800

    1.添加订单列表获取

--- anjia/js/c_usercenter.js
+++ anjia/js/c_usercenter.js
@@ -10,6 +10,10 @@ $(function(){
 	//g.page = Utils.getQueryString("p") - 0;
 	g.httpTip = new Utils.httpTip({});
 
+	g.totalPage = 1;
+	g.currentPage = 1;
+	g.pageSize = 10;
+
 	//验证登录状态
 	var loginStatus = Utils.getUserInfo();
 	if(!loginStatus){
@@ -18,6 +22,11 @@ $(function(){
 	}
 	else{
 		getUserInfo();
+		//获取订单列表
+		getUserOrderList();
+
+		//获取订单状态
+		sendGetUserInfoDicHttp();
 	}
 
 
@@ -157,4 +166,306 @@ $(function(){
 			return false;
 		}
 	}
+
+
+	//获取用户信息字典信息
+	function sendGetUserInfoDicHttp(){
+		g.httpTip.show();
+		var url = Base.serverUrl + "baseCodeController/getBaseCodeByParents";
+		var condi = {};
+		condi.parents = "1005";
+		$.ajax({
+			url:url,
+			data:condi,
+			type:"POST",
+			dataType:"json",
+			context:this,
+			success: function(data){
+				console.log("sendGetUserInfoDicHttp",data);
+				var status = data.success || false;
+				if(status){
+					var obj = data.obj || {};
+					changeSelectHtml(obj);
+				}
+				else{
+					var msg = data.message || "获取用户信息字典数据失败";
+					Utils.alert(msg);
+				}
+				g.httpTip.hide();
+			},
+			error:function(data){
+				g.httpTip.hide();
+			}
+		});
+	}
+
+	function changeSelectHtml(obj){
+		var parents = ["1005"];
+		var ids = ["orderstatus"];
+		for(var i = 0,len = parents.length; i < len; i++){
+			var data = obj[parents[i]] || {};
+			var option = [];
+			option.push('<option value="">全部订单</option>');
+			for(var k in data){
+				var id = k || "";
+				var name = data[k] || "";
+				option.push('<option value="' + id + '">' + name + '</option>');
+			}
+			$("#" + ids[i]).html(option.join(''));
+		}
+	}
+
+	function getUserOrderList(){
+		var condi = {};
+		condi.login_token = g.login_token;
+		condi.customerId = g.customerId;
+		condi.status = $("#orderstatus").val() || "";
+		condi.currentPageNum = g.currentPage;
+		condi.pageSize = g.pageSize;
+
+		sendGetUserOrderListHttp(condi);
+	}
+
+	function sendGetUserOrderListHttp(condi){
+		g.httpTip.show();
+		var url = Base.serverUrl + "order/queryOrdersController";
+		$.ajax({
+			url:url,
+			data:condi,
+			type:"POST",
+			dataType:"json",
+			context:this,
+			success: function(data){
+				console.log("sendGetUserOrderListHttp",data);
+				var status = data.success || false;
+				if(status){
+					changeOrderListHtml(data);
+				}
+				else{
+					var msg = data.message || "获取用户订单失败";
+					Utils.alert(msg);
+				}
+				g.httpTip.hide();
+			},
+			error:function(data){
+				g.httpTip.hide();
+			}
+		});
+	}
+
+	function changeOrderListHtml(data){
+
+		var html = [];
+
+		html.push('<table class="order-table" cellpadding="0" cellspacing="0">');
+		html.push('<tr>');
+		html.push('<th width="160">订单编号</th>');
+		html.push('<th width="129">合同编号</th>');
+		html.push('<th width="180">套餐名称</th>');
+		html.push('<th width="130">价格</th>');
+		html.push('<th width="140">订单状态</th>');
+		html.push('<th width="150">最近待还</th>');
+		html.push('<th width="80">总期数</th>');
+		html.push('<th>操作</th>');
+		html.push('</tr>');
+		var obj = data.list || [];
+		for(var i = 0,len = obj.length; i < len; i++){
+			var d = obj[i];
+			var orderId = d.orderId || "";
+			var contractNo = d.contractNo || "";
+			var packageName = d.packageName || "";
+			var packageMoney = d.packageMoney || "";
+			var statusDes = d.statusDes || "";
+			var status = d.status || "";
+			var fenQiTimes = d.fenQiTimes || 0;
+			var noRepaymentTimes = d.noRepaymentTimes || 0;
+
+			html.push('<tr>');
+			html.push('<td>' + orderId + '</td>');
+			html.push('<td>' + contractNo + '</td>');
+			html.push('<td>' + packageName + '</td>');
+			html.push('<td>' + packageMoney + '元</td>');
+			html.push('<td>' + statusDes + '</td>');
+			html.push('<td>' + fenQiTimes + '</td>');
+			html.push('<td>' + noRepaymentTimes + '期</td>');
+			if(status == "100501"){
+				html.push('<td><a href="/anjia/mystaging.html?id=' + orderId + '">编辑</a><a href="javascript:deleteOrderById(\'' + orderId + '\')">删除</a></td>');
+			}
+			else{
+				html.push('<td><a href="/anjia/order-detail.html">查看</a></td>');
+			}
+			html.push('</tr>');
+		}
+		html.push('</table>');
+
+		var pobj = data.obj || {};
+		var page = countListPage(pobj);
+		html.push(page);
+
+		$("#orderlist").html(html.join(''));
+
+		$("#orderlistpage a").bind("click",pageClick);
+	}
+
+	function countListPage(data){
+		var html = [];
+		g.totalPage = Math.ceil(data.totalRowNum / g.pageSize);
+		//g.totalPage = 1;
+		//g.currentPage = 1;
+		html.push('<div id="orderlistpage" class="ui-pager">');
+		html.push('<a href="javascript:void(0)" class="page-pre-end">&nbsp;</a>');
+		html.push('<a href="javascript:void(0)" class="page-pre">&nbsp;</a>');
+
+		if(g.totalPage > 10){
+			if(g.currentPage >= 10){
+				var css = "background: #89c997;color: #ffffff;border: 1px solid #89c997";
+
+				if((g.totalPage - g.currentPage) >= 5){
+					html.push('<a href="javascript:void(0)" >' + (g.currentPage - 4) + '</a>');
+					html.push('<a href="javascript:void(0)" >' + (g.currentPage - 3) + '</a>');
+					html.push('<a href="javascript:void(0)" >' + (g.currentPage - 2) + '</a>');
+					html.push('<a href="javascript:void(0)" >' + (g.currentPage - 1) + '</a>');
+					html.push('<a href="javascript:void(0)" style="' + css + '">' + (g.currentPage) + '</a>');
+				}
+				else{
+					//末尾少于5页
+					var len = 9 - (g.totalPage - g.currentPage);
+					for(var j = len; j >= 0; j--){
+						if(j == 0){
+							html.push('<a href="javascript:void(0)" style="' + css + '">' + (g.currentPage) + '</a>');
+						}
+						else{
+							html.push('<a href="javascript:void(0)" >' + (g.currentPage - j) + '</a>');
+						}
+					}
+				}
+				for(var i = 1; i < 6; i++){
+					var np = g.currentPage + i;
+					if(np <= g.totalPage){
+						html.push('<a href="javascript:void(0)" >' + np + '</a>');
+					}
+					else{
+						break;
+					}
+				}
+
+			}
+			else{
+				for(var i = 0; i < 10; i++){
+					var css = (i + 1) == g.currentPage ? "background: #89c997;color: #ffffff;border: 1px solid #89c997;" : "";
+					html.push('<a href="javascript:void(0)" style="' + css + '">' + (i + 1) + '</a>');
+				}
+			}
+		}
+		else{
+			for(var i = 0; i < g.totalPage; i++){
+				var css = (i + 1) == g.currentPage ? "background: #89c997;color: #ffffff;border: 1px solid #89c997;" : "";
+				html.push('<a href="javascript:void(0)" style="' + css + '">' + (i + 1) + '</a>');
+			}
+		}
+
+		html.push('<a href="javascript:void(0)" class="page-next">&nbsp;</a>');
+		html.push('<a href="javascript:void(0)" class="page-next-end">&nbsp;</a>');
+		html.push('</div>');
+
+		return html.join('');
+	}
+
+	function pageClick(evt){
+		var index = $(this).index();
+		var text = $(this).text() - 0 || "";
+		if(text !== ""){
+			if(g.currentPage === text){
+				Utils.alert("当前是第" + text + "页");
+				return;
+			}
+			else{
+				g.currentPage = text;
+			}
+		}
+		else{
+			var cn = $(this)[0].className;
+			switch(cn){
+				case "page-pre-end":
+					//第一页
+					if(g.currentPage == 1){
+						Utils.alert("当前是第一页");
+						return;
+					}
+					else{
+						g.currentPage = 1;
+					}
+				break;
+				case "page-pre":
+					//前一页
+					if(g.currentPage > 1){
+						g.currentPage--;
+					}
+					else{
+						Utils.alert("当前是第一页");
+						return;
+					}
+				break;
+				case "page-next":
+					//后一页
+					g.currentPage++;
+				break;
+				case "page-next-end":
+					//最后一页
+					g.currentPage = g.totalPage;
+				break;
+			}
+		}
+
+		if(g.currentPage <= g.totalPage){
+			getUserOrderList();
+		}
+		else{
+			Utils.alert("当前是最后一页");
+		}
+	}
+
+	function deleteOrderById(id){
+		g.httpTip.show();
+		var condi = {};
+		condi.orderId = id;
+		var url = Base.serverUrl + "order/queryOrdersController";
+		$.ajax({
+			url:url,
+			data:condi,
+			type:"POST",
+			dataType:"json",
+			context:this,
+			success: function(data){
+				console.log("deleteOrderById",data);
+				var status = data.success || false;
+				if(status){
+				}
+				else{
+					var msg = data.message || "删除订单数据失败";
+					Utils.alert(msg);
+				}
+				g.httpTip.hide();
+			},
+			error:function(data){
+				g.httpTip.hide();
+			}
+		});
+	}
+
+
+	window.deleteOrderById = deleteOrderById;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
