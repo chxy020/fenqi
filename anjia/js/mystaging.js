8932b0e08b01bb36816f98c3d07c30e07bfc13d3
xiaolibier <xiaolibier@126.com>
2015-12-07 09:40:33 +0800

    解决ie 分期时 不能上传图片的问题 原因是 indexOf()不支持

--- anjia/js/mystaging.js
+++ anjia/js/mystaging.js
@@ -1415,6 +1415,28 @@ $(function(){
 		var condi = {};
 		condi.login_token = g.login_token;
 		condi.orderId = g.orderId;
+		/*  */
+		if (!Array.prototype.indexOf)
+		{
+		  Array.prototype.indexOf = function(elt /*, from*/)
+		  {
+			var len = this.length >>> 0;
+			var from = Number(arguments[1]) || 0;
+			from = (from < 0)
+				 ? Math.ceil(from)
+				 : Math.floor(from);
+			if (from < 0)
+			  from += len;
+			for (; from < len; from++)
+			{
+			  if (from in this &&
+				  this[from] === elt)
+				return from;
+			}
+			return -1;
+		  };
+		}
+/*  */
 		if(g.uploadMark.indexOf(0) == -1){
 			sendSetOrderCompleteHttp(condi);
 		}
