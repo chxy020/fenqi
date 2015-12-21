bbc7c20aab66a1ece117334ed044e167b5554b05
xiaolibier <xiaolibier@126.com>
2015-12-21 17:15:11 +0800

    修改注册来源 不经过生活家和朗润的人  归为德维

--- anjia/js/reg.js
+++ anjia/js/reg.js
@@ -241,6 +241,7 @@ $(function(){
 		var platform = 3;
 		g.city = Utils.offLineStore.get("curCity",false) || "";
 		var city = g.city || "";
+		if(company == ""){company = "20150901"; }
 		condi.company = company;//传城市 德维-20150901，生活家-20150901000001，朗润-20150901000002
 		condi.platform = platform;//平台(0-wap 1-android 2-ios 3-pc)
 		condi.city = city;//城市
