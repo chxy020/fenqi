411532e2ee0d6f303e59d90f3e73f0c61a128b5e
xiaolibier <xiaolibier@126.com>
2015-12-30 17:28:07 +0800

    支付页面 增加对自动对工商银行的付款 进行判断

--- anjia/js/cardpay.js
+++ anjia/js/cardpay.js
@@ -14,15 +14,15 @@ $(function(){
 	g.httpTip = new Utils.httpTip({});
 	g.couponId = Utils.getQueryString("id") || "";
 	g.repaymentRecordId = Utils.getQueryString("recordId") || "";
-	g.price = Utils.getQueryString("p") - 0 || 0;
-
+	g.price = Utils.getQueryString("p") - 0 || 0;	
+	g.breakUp = false;
 	g.codeImg = $("#imgcodebtn")[0];
 	g.guid = Utils.getGuid();
 	g.customerId = "";
 	g.bindBankCardId = "";
 	g.playCondi = {};
 	g.payId = "";
-
+	g.paidMoney = 0;
 
 	//获取图形验证码
 	sendGetImgCodeHttp();
@@ -34,11 +34,12 @@ $(function(){
 		location.replace("/anjia/login.html");
 	}
 	else{
+		serchRepaymentRecordByRepaymentRecordId();
 		getUserInfo();
 		//
 		changeOrderInfoHtml();
-
 		sendGetBindBankCardByCustomerId();
+		
 	}
 
 	$("#imgcodebtn").bind("click",sendGetImgCodeHttp);
@@ -67,6 +68,38 @@ $(function(){
 			g.customerId = obj.customerId || "";
 			g.userPhone = obj.phoneNumber || "";
 		}
+	}
+	
+		//查询已支付金额
+	function serchRepaymentRecordByRepaymentRecordId(){
+		var url = Base.serverUrl + "order/getRepaymentRecordByRepaymentRecordId";
+		var condi = {};
+		condi.login_token = g.login_token;
+		condi.repaymentRecordId = g.repaymentRecordId;
+		$.ajax({
+			url:url,
+			type:"POST",
+			data:condi,
+			dataType:"json",
+			context:this,
+			success: function(data){				
+				var status = data.success || false;
+				if(status){
+					//请求成功
+					var obj = data.obj || [];
+					var paidMoney = obj.paidMoney || 0;
+					g.paidMoney = paidMoney;
+					g.price2 = g.price - paidMoney || 0 ;
+					
+				}
+				else{
+					var msg = data.message || "查询已支付金额失败";					
+					Utils.alert(msg);
+				}
+			},
+			error:function(data){
+			}
+		});
 	}
 
 	function changeOrderInfoHtml(){
@@ -116,12 +149,15 @@ $(function(){
 
 			var bankCardTop = obj.bankCardTop || "";
 			var bankCardLast = obj.bankCardLast || "";
-
+			//触发支付请求
+			
+			
 			var html = [];
 			for(var i = 0,len = list.length; i < len; i++){
 				var d = list[i] || {};
 				var bankType = d.bankType || "";
 				bankType = bankType.toLowerCase();
+				//if(i == 0){bankType = "icbc";}//测试工商银行
 				var bankCard = d.bankCard || "";
 				bankCard = "****" + bankCard.substring(bankCard.length - 4);
 
@@ -132,19 +168,29 @@ $(function(){
 					html.push('<div class="bank-card-inf selected" style="margin:0px 15px;">');
 					html.push('<span class="bank-card-inf-num">' + bankCard + '</span>');
 					html.push('<img src="' + logo + '" class="bank-card-inf-logo" />');
-					html.push('</div>');
-					html.push('<input type="radio" name="bindcardradio" class="common-radio" checked="checked" />');
+					html.push('</div>');					
+					if(bankType == "icbc"){
+						html.push('<input type="radio" name="bindcardradio" class="common-radio common-radio_ccb" checked="checked" />');
+					}else{
+						html.push('<input type="radio" name="bindcardradio" class="common-radio" checked="checked" />');
+					}
 					html.push('</label>');
 					html.push('</li>');
+					/* 判断银行卡是不是工商银行 */
+					if(bankType == "icbc"){breakUp();}else{sendPlayBindRequest(bankCardTop,bankCardLast);}
 				}
 				else{
 					html.push('<li style="height: 30px;">');
 					html.push('<label style="width:310px;  text-align: left;">');
 					html.push('<div class="bank-card-inf" style="margin:0px 15px;">');
 					html.push('<span class="bank-card-inf-num">' + bankCard + '</span>');
-					html.push('<img src="../res/images/bank-logo.jpg" class="bank-card-inf-logo" />');
+					html.push('<img src="' + logo + '" class="bank-card-inf-logo" />');
 					html.push('</div>');
-					html.push('<input type="radio" name="bindcardradio" class="common-radio" />');
+					if(bankType == "icbc"){
+						html.push('<input type="radio" name="bindcardradio" class="common-radio  common-radio_ccb" />');
+					}else{
+						html.push('<input type="radio" name="bindcardradio" class="common-radio" />');
+					}	
 					html.push('</label>');
 					html.push('</li>');
 				}
@@ -152,14 +198,62 @@ $(function(){
 
 			$("#mycardlist").html(html.join(''));
 			$('.common-radio').yyptRadio();
-
-			//触发支付请求
-			sendPlayBindRequest(bankCardTop,bankCardLast);
+			addClick_div();
 		}
 	}
+	//判断选中的支付方式是不是工商银行
+	function addClick_div(){
+		$(".radio-bg").click(function(){
+			if($(".common-radio_ccb").attr("checked") == "checked"){
+				g.breakUp = true;breakUp();
+			}else{
+				g.breakUp = false;$(".sign-item.fenbi,.sign-item.tip,.sign-item.tip2").fadeOut(0);sendGetBindBankCardByCustomerId2();
+			}
+			 return false;//防止冒泡
+		})
+	}
 
+	/* 12-25 */
+	//单独发送支付请求
+	function sendGetBindBankCardByCustomerId2(xiane){
+		g.httpTip.show();
+		var url = Base.serverUrl + "payPc/getBindBankCardByCustomerId";
+		var condi = {};
+		condi.login_token = g.login_token;
+		condi.customerId = g.customerId;
+		$.ajax({
+			url:url,
+			data:condi,
+			type:"POST",
+			dataType:"json",
+			context:this,
+			success: function(data){
+				console.log("sendGetBindBankCardByCustomerId",data);
+				var status = data.success || false;
+				if(status){
+					var list = data.list || [];
+					var obj = list[0] || {};
+					var bankCardTop = obj.bankCardTop || "";
+					var bankCardLast = obj.bankCardLast || "";
+				//触发支付请求
+					sendPlayBindRequest(bankCardTop,bankCardLast,xiane);
+				}
+				else{
+					var msg = data.message || "获取绑定银行卡失败";
+					Utils.alert(msg);
+				}
+				g.httpTip.hide();
+			},
+			error:function(data){
+				g.httpTip.hide();
+			}
+		});
+	}
+	/* 12-25 */
+	
 	//请求支付
-	function sendPlayBindRequest(bankCardTop,bankCardLast){
+	function sendPlayBindRequest(bankCardTop,bankCardLast,xiane){
+		var xiane = xiane || true;
 		var url = Base.serverUrl + "payPc/sendBindRequest";
 		g.httpTip.show();
 		var condi = {};
@@ -169,6 +263,7 @@ $(function(){
 		condi.bankCardTop = bankCardTop;
 		condi.bankCardLast = bankCardLast;
 		condi.couponId = g.couponId;
+		condi.isFullPay = xiane;//传送支付金额
 		$.ajax({
 			url:url,
 			type:"POST",
@@ -184,6 +279,7 @@ $(function(){
 				}
 				else{
 					var msg = data.message || "支付请求失败";
+					if(msg == "单卡超过单笔支付限额"){msg = "所选银行卡余额不足"}
 					Utils.alert(msg);
 				}
 				g.httpTip.hide();
@@ -194,6 +290,8 @@ $(function(){
 		});
 	}
 
+
+	
 	//获取支付验证码
 	function getValidCode(){
 		var img_validate_code = $("#inputimgcode").val() || "";
@@ -324,7 +422,13 @@ $(function(){
 	function showPayTip(){
 		layer.alert('支付中...', {icon: 4,closeBtn: 0}, function(index){
 			layer.close(index);
-			location.href = "/anjia/usercenter.html";
+			if(g.price2 > 5 && g.price2 <= 10 && g.breakUp){//分笔支付方式
+				g.price2 -= 5;
+				$(".form_input .form_input1,.form_input .fenbi_step2").fadeOut(0);
+				sendGetBindBankCardByCustomerId2(g.price2);
+			}else{
+				location.href = "/anjia/usercenter.html";
+			}			
 		});
 		$(".layui-layer-btn").hide();
 
@@ -349,8 +453,12 @@ $(function(){
 					//{"success":true,"obj":{支付对象,status:0失败 1成功 2未处理 3处理中 ,errorcode:错误代码,errormsg:错误信息},"list":[],"message":"","code":null,"token":“”}
 					var status = data.obj.status - 0;
 					if(status == 1){
-						$(".layui-layer-btn").show();
-						$(".layui-layer-content").html('<i class="layui-layer-ico layui-layer-ico1"></i>支付成功');
+						$(".layui-layer-btn").show();						
+						if(g.price2 > 5 && g.price2 <= 10 && g.breakUp){//分笔支付
+							$(".layui-layer-content").html('<i class="layui-layer-ico layui-layer-ico1"></i>支付成功,请继续支付剩余金额！');		
+						}else {
+							$(".layui-layer-content").html('<i class="layui-layer-ico layui-layer-ico1"></i>支付成功');
+						}
 						//消除优惠券
 					}
 					else if(status == 0){
@@ -379,8 +487,36 @@ $(function(){
 	}
 
 
-
-
+/* 拆分金额计算器 */
+	function breakUp(){		
+		var money = g.price || 0;
+		var money2 = g.price || 0;
+		var xiane = 5;		
+		var i = 1;
+		if(money2 > xiane && money2 <= xiane*2){
+			while(money2 > xiane){
+				$(".form_input .form_input"+i+"").attr("value",xiane).fadeIn(0);
+				money2 -= xiane;i++;
+				if(money2 > xiane ){continue;}else{$(".form_input .form_input"+i+"").attr("value",money2).fadeIn(0);break;}
+			}
+		}
+		if(money > xiane && money <= xiane*2){
+			$(".sign-item.fenbi,.sign-item.tip").fadeIn(0);$(".sign-item.tip2").fadeOut(0);
+			g.breakUp = true;
+			if(g.paidMoney > 0){
+				xiane = g.price - g.paidMoney;g.breakUp = false;			
+				$(".form_input .form_input1,.form_input .fenbi_step2").fadeOut(0);
+				$(".form_input .form_input2").attr("value",xiane).fadeIn(0);
+			}//判断如果已经支付过一部分 则不分笔支付		
+			sendGetBindBankCardByCustomerId2(xiane);//支付请求
+			
+		}else if(money > xiane*2){
+			$(".sign-item.tip2").fadeIn(0);g.breakUp = false;sendGetBindBankCardByCustomerId2();
+		}else{
+			$(".sign-item.fenbi,.sign-item.tip,.sign-item.tip2").fadeOut(0);g.breakUp = false;sendGetBindBankCardByCustomerId2();
+		}
+		
+	}
 
 
 
@@ -451,5 +587,7 @@ $(function(){
 		$("#bankCode").html(option.join(''));
 	}
 
-
+	window.addClick_div = addClick_div;
+	window.breakUp = breakUp;
+	window.sendGetBindBankCardByCustomerId2 = sendGetBindBankCardByCustomerId2;
 });
