fa73a000491190b1a28ca5e5364f74a81bb5538e
xiaolibier <xiaolibier@126.com>
2016-03-22 10:37:39 +0800

    修改我的额度 在其他页面存在无法锁定的bug

--- anjia/js/c_usercenter.js
+++ anjia/js/c_usercenter.js
@@ -27,7 +27,6 @@ $(function(){
 		location.replace("/anjia/login.html");
 	}
 	else{
-		yuqi_message_fuc2();
 		getUserInfo();
 		//获取订单列表
 		getUserOrderList(true);
@@ -121,59 +120,7 @@ $(function(){
 		
 		
 	}
-	/* 每次进个人中心实时监测是否有逾期 违约订单 然后控制我的额度 是否可用 */
-	function yuqi_message_fuc2(){
-		//order/selectCustomerOrderNextRepaymentRecords
-		var condi = {};
-		condi.login_token = g.login_token;
-		var url = Base.serverUrl + "order/selectCustomerOrderNextRepaymentRecords";//修改之前queryOrdersController
-		$.ajax({
-			url:url,
-			data:condi,
-			type:"POST",
-			dataType:"json",
-			context:this,
-			success: function(data){
-				var success = data.success || false;
-				if(success){
-					var obj = data.list || [];
-					var yuqi = "",weiyue = "",j_yuqi = "";
-					g.yuqi_weiyue = false;
-					for(var i = 0,len = obj.length; i < len; i++){
-						var d = obj[i];
-						var orderStatus =d.orderStatus || "";
-						var orderId = d.orderId || "";
-						var repaymentTimes = d.repaymentTimes || "";
-						var repayResidueDay = d.repayResidueDay || 0;
-						
-						if(orderStatus == "100510"){
-							yuqi +='1';
-						}
-						else if(orderStatus == "100511"){
-							weiyue += '1';
-						}							
-					}
-					if(yuqi != "" || weiyue != ""){
-						g.yuqi_weiyue = true;
-					}
-					if(g.yuqi_weiyue){
-						$("#userleft_abtn").removeAttr("href").addClass("usercenter_a");
-					}else{
-						$("#userleft_abtn").attr("href","/anjia/usercenter.html?item=5&ostatus=100500").removeClass("usercenter_a");
-					}
-					
-				}
-				else{
-					var msg = data.message || "获取逾期信息失败";
-					Utils.alert(msg);
-				}
-			},
-			error:function(data){
-			}
-		});
-		
-		
-	}
+
 		/* 关闭窗口 */
 	function close_box(event){
 		$(this).parents(".yuqi_box").slideUp(300);
