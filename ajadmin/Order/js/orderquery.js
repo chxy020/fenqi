5138c881af867ed75e426528eedee36ece73fb07
hmgx <350078238@qq.com>
2016-04-12 11:20:34 +0800

    恢复 订单查询 查看订单页

--- ajadmin/Order/js/orderquery.js
+++ ajadmin/Order/js/orderquery.js
@@ -456,7 +456,8 @@ $(function(){
 	window.deleteOrderById = deleteOrderById;
 
 	window.ViewOrder = function (OrderId){
-		Hmgx.openWin("ModifyOrder_only.html?orderid=" + OrderId );
+		Hmgx.openWin("ViewOrderDetail.html?orderid=" + OrderId );
+		//Hmgx.openWin("ModifyOrder_only.html?orderid=" + OrderId );
 	}
 
 });
